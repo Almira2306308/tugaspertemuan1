a0c4dfae10d63c15e4b80650202b79bd2fe64c82
Almira2306308 <notionalmira@gmail.com>
2025-02-03 20:12:05 +0700

    Add files via upload

---
+++ PertemuanSatu.java
@@ -0,0 +1,8 @@
+public class PertemuanSatu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Almira Rahma Fadhila - 2306308");
+        System.out.println("Saya Senang Belajar Java");
+    }
+}
